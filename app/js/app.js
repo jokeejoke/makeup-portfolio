f5c0fbfe340574fa2b1c83d0109af9fdf06d170b
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2020-12-23 16:13:36 +0600

    add about page v2

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,4 @@
 //import $ from 'jquery'; window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
-import WOW from 'wow.js'
 import scrollSpy from 'simple-scrollspy'
 import scrollPage from 'page-scroll-to-id'
 
@@ -11,23 +10,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	scrollSpy('.header', {
 		offset: 128
 	})
-
-	const wow = new WOW(
-		{
-			boxClass:     'wow',      // animated element css class (default is wow)
-			animateClass: 'animated', // animation css class (default is animated)
-			offset:       0,          // distance to the element when triggering the animation (default is 0)
-			mobile:       true,       // trigger animations on mobile devices (default is true)
-			live:         true,       // act on asynchronously loaded content (default is true)
-			callback:     function(box) {
-				// the callback is fired every time an animation is started
-				// the argument that is passed in is the DOM node being animated
-			},
-			scrollContainer: null,    // optional scroll container selector, otherwise use window,
-			resetAnimation: true,     // reset animation on end (default is true)
-		}
-	);
-	wow.init();
 
 	window.onscroll = function() {
 		if (window.pageYOffset < 100) {
